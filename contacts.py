75f84788518fc8fc06cefbc82a6544e9fe4d685b
lmpizarro <lmpbici@gmail.com>
2015-05-13 20:41:42 +0000

    encode utf8

--- contacts.py
+++ contacts.py
@@ -6,4 +6,4 @@ class Contacts():
 
     def add_contact (self, name, email, telefono, mensaje):
         self.contacts.append ({'nombre':name, 'email':email, 'phone':telefono, 'mensaje':mensaje}) 
-        print name.decode("utf-8"), email.decode("utf-8"), telefono.decode("utf-8"), mensaje.decode("utf-8")
+        #print name.decode("utf-8"), email.decode("utf-8"), telefono.decode("utf-8"), mensaje.decode("utf-8")
