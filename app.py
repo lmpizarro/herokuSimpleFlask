55e8a67e01d9a5ec6913bf2526b93f2a34a3aea5
lmpizarro <lmpbici@gmail.com>
2015-06-03 12:47:35 +0000

    app.py responsivefixed

--- app.py
+++ app.py
@@ -93,8 +93,7 @@ def clean_blog_thank(contact_name):
     return render_template('start_boot_strap/clean_blog/thank.html', contact=contact_name)
 
 @app.route("/responsive")
-def clean_blog():
-    all_posts = db_posts.get_all()
+def responsive():
     return render_template('start_boot_strap/responsive/index.html', posts=all_posts)
 
 
